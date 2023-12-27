2d760fb0f74e2593a8ead744018fbc2e85ac0618
Juan Rogel Navarro <juan.rogel@gmail.com>
2023-12-27 13:11:41 +0100

    Add healthCheck service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
